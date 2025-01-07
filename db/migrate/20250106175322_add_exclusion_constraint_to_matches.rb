f903d47d52fdb938ffd1ae0b4272bdcdcbf0fd1d
FrancoARossi <rossifrancoa@gmail.com>
2025-01-07 13:09:36 -0300

    fix: make migration reversible

--- db/migrate/20250106175322_add_exclusion_constraint_to_matches.rb
+++ db/migrate/20250106175322_add_exclusion_constraint_to_matches.rb
@@ -1,5 +1,5 @@
 class AddExclusionConstraintToMatches < ActiveRecord::Migration[8.0]
-  def change
+  def up
     # only supported in PostgreSQL
     enable_extension 'btree_gist'
 
@@ -22,5 +22,22 @@ class AddExclusionConstraintToMatches < ActiveRecord::Migration[8.0]
         tsrange(start_time, end_time) WITH &&
       );
     SQL
+  end
+
+  def down
+    remove_index :matches, name: 'index_matches_on_player1_and_time'
+    remove_index :matches, name: 'index_matches_on_player2_and_time'
+
+    execute <<-SQL
+      ALTER TABLE matches
+      DROP CONSTRAINT no_overlap_for_player1;
+    SQL
+
+    execute <<-SQL
+      ALTER TABLE matches
+      DROP CONSTRAINT no_overlap_for_player2;
+    SQL
+
+    disable_extension 'btree_gist'
   end
 end
